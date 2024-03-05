eae4fff09601b26060d4042dad9504b4f0926c10
janithwijethunga <janithwijethunga68@gmail.com>
2024-03-05 08:29:45 +0530

    do backend

--- frontend/src/screens/Dashboard.js
+++ frontend/src/screens/Dashboard.js
@@ -1,29 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 function Dashboard() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [mem, setMem] = useState([]);
+
+  async function addMember() {
+    const newMember = { name, email, password, phone, address };
+    console.log(newMember);
+
+    try {
+      const result = await axios.post("http://localhost:5001/api/mem/dashboard", newMember);
+      console.log(result.data);
+      alert("Member added successfully");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/api/mem/getallmem");
+      setMem(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <table className=" border-8 border-gray-500/100 mx-44 mt-44 ml-56">
+    <table className="border-8 border-gray-500/100 mx-44 mt-44 ml-56">
       <thead className="border-8">
         <tr>
           <th className="px-8 py-4">ID</th>
           <th className="px-8 py-4">Name</th>
           <th className="px-8 py-4">Email</th>
-          <th className=" py-4">Password</th>
-          <th className=" py-4">Phone Number</th>
-          <th className=" py-4">Address</th>
+          <th className="px-8 py-4">Password</th>
+          <th className="px-8 py-4">Phone Number</th>
+          <th className="px-8 py-4">Address</th>
+          <th className="px-8 py-4">Actions</th>
         </tr>
       </thead>
 
       <tbody>
-        <tr>
-          <td>001</td>
-          <td>Janith</td>
-          <td>janith@gmail.com</td>
-          <td>123456</td>
-          <td>0712345678</td>
-          <td>Haguranketha, Kandy</td>
-        </tr>
-        
+        {mem.map((member) => (
+          <tr key={member._id} className="bg-white dark:bg-table-row hover:tablerow-hover dark:hover:bg-tablerow-hover">
+            <td>{member._id}</td>
+            <td>{member.name}</td>
+            <td>{member.email}</td>
+            <td>{member.password}</td>
+            <td>{member.phone}</td>
+            <td>{member.address}</td>
+            <td>
+              <Link to="#">
+                <button className="btn1 mr-3">Edit</button>
+              </Link>
+              <Link to="#">
+                <button className="btn1">Delete</button>
+              </Link>
+            </td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
